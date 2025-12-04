bf2de67cedf4e875cd6c189a2a93d442ad5f683f
erandesamadhan2003 <erandesamadhan2003@gmail.com>
2025-12-04 16:51:36 +0530

    style

--- App.tsx
+++ App.tsx
@@ -2,11 +2,11 @@ import './global.css';
 import { Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View className="flex-1 items-center justify-center bg-red-700">
-      <Text className="text-xl font-bold text-yellow-500">
-        Welcome to Nativewind!
-      </Text>
-    </View>
-  );
+    return (
+        <View className="flex-1 items-center justify-center bg-red-700">
+            <Text className="text-xl font-bold text-yellow-500">
+                Welcome to Nativewind!
+            </Text>
+        </View>
+    );
 }
