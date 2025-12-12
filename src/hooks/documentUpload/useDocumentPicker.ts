ab231af16d6de4526b2ec08ee4a07861be5e911e
athPATIL247 <atharvaspatil247@gmail.com>
2025-12-12 20:42:54 +0530

    Type fix

--- src/hooks/documentUpload/useDocumentPicker.ts
+++ src/hooks/documentUpload/useDocumentPicker.ts
@@ -1,4 +1,4 @@
-import { pick, types, isCancel } from "@react-native-documents/picker";
+import { pick, types, isErrorWithCode, errorCodes } from "@react-native-documents/picker";
 import { UploadFileType } from "../../types/student/studentProfile.types";
 
 export const useDocumentPicker = () => {
@@ -13,11 +13,13 @@ export const useDocumentPicker = () => {
 
             return {
                 uri: res.uri,
-                name: res.name || res.fileName || "document.pdf",
+                name: res.name || "document.pdf",
                 type: res.type || "application/pdf",
             };
         } catch (err: any) {
-            if (isCancel?.(err)) return null;
+            if (isErrorWithCode(err) && err.code === errorCodes.OPERATION_CANCELED) {
+                return null;
+            }
             throw "Failed to pick document";
         }
     };
